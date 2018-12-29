17f17a92cbda2a2d9da25ba09f4ed812c5a8a9fa
Weiran Zhang <weiran@zhang.me.uk>
2018-12-29 14:21:48 +0000

    Initial Commit

---
+++ TVVLCPlayerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TVVLCPlayerTest
+//
+//  Created by Weiran Zhang on 29/12/2018.
+//  Copyright © 2018 Weiran Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
